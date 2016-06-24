415e26f6210238ad0178e022e7a86999214e3b64
maikooono <heyasemai@gmail.com>
2016-06-24 13:03:34 +0900

    first commit

---
+++ Assets/Source/Editor/RunMoreUnity.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Diagnostics;
+using UnityEditor;
+
+public class RunMoreUnity {
+
+	#if UNITY_EDITOR_OSX
+
+	[MenuItem("File/Start One More Unity")]
+	private static void StartNewUnity() {
+		var p = new ProcessStartInfo();
+		p.FileName = "/Applications/Unity/Unity.app/Contents/MacOS/Unity";
+		p.UseShellExecute = true;
+		p.CreateNoWindow = true;
+		Process.Start(p);
+	}
+
+	#endif
+}
